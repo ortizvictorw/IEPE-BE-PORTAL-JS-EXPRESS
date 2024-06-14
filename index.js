75a1def27f88c810011a8f774f329ce570a0ddfe
@vortiz-agropago <vortiz@agropago.com>
2024-06-14 08:16:33 -0300

    basic

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const indexRouter = require('./routes/index');
