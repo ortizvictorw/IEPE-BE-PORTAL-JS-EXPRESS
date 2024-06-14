89d206ab371f73ff5ab3f72cbae0c1a017290aef
@vortiz-agropago <vortiz@agropago.com>
2024-06-14 09:51:36 -0300

    index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const memberRouter = require('./routes/member');
 const db = require('./configurations/db.config');
+const MongoMemberRepository = require('./repositories/MongoMemberRepository');
+const memberRepository = new MongoMemberRepository();
 
 dotenv.config({ path: '.env' });
 
@@ -20,8 +21,69 @@ const app = express();
 app.use(express.json({ extended: false }));
 app.use(cors());
 
+// Servicios
+const createMember = async (req, res) => {
+    try {
+        const member = req.body;
+        const savedMember = await memberRepository.save(member);
+        res.status(201).json(savedMember);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const getMembers = async (_req, res) => {
+    try {
+        const members = await memberRepository.find();
+        console.log(members);
+        res.status(200).json(members);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const getMemberById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const member = await memberRepository.findById(id);
+        if (member) {
+            res.status(200).json(member);
+        } else {
+            res.status(404).json({ message: 'Member not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const updateMember = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const member = req.body;
+        member.dni = id;
+        const updatedMember = await memberRepository.update(member);
+        res.status(200).json(updatedMember);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const deleteMember = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await memberRepository.delete(id);
+        res.status(200).json({ message: result });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
 // Rutas
-app.use('/members', memberRouter);
+app.post('/members', createMember);
+app.get('/members', getMembers);
+app.get('/members/:id', getMemberById);
+app.put('/members/:id', updateMember);
+app.delete('/members/:id', deleteMember);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
